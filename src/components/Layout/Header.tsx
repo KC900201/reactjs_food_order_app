eaa9c84e4082de4fbf6cf2b7d99d5972f729195f
KC Ng <nkc900201@gmail.com>
2023-08-13 16:45:34 +0900

    Remove original css setup and install new packages (prettier, eslint, styled-component), create new folders as well as new components

---
+++ src/components/Layout/Header.tsx
@@ -0,0 +1,53 @@
+import * as React from 'react'
+import { Fragment } from 'react'
+import styled from 'styled-components'
+
+import mealsImage from '../../assets/meals.jpg'
+import { transform } from 'typescript'
+
+const HeaderProp = styled.header`
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 5rem;
+  background-color: #8a2b06;
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 0 10%;
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.25);
+  z-index: 10;
+`
+const ImageSection = styled.div`
+  width: 100%;
+  height: 25rem;
+  z-index: 0;
+  overflow: hidden;
+`
+
+const Header = (props: any) => {
+  return (
+    <Fragment>
+      <HeaderProp>
+        <h1>React Meals</h1>
+        <button>Cart</button>
+      </HeaderProp>
+      <ImageSection>
+        <img
+          src={mealsImage}
+          alt="meals"
+          width="110%"
+          height="100%"
+          style={{
+            objectFit: 'cover',
+            transform: 'rotateZ(-5deg) translateY(-4rem) translateX(-1rem)',
+          }}
+        />
+      </ImageSection>
+    </Fragment>
+  )
+}
+
+export default Header
---
+++ src/components/Layout/HeaderCartButton.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+
+// WIP (8/13/2023)
+const HeaderCartButton = (props: any) => {}
+
+export default HeaderCartButton

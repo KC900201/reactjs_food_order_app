9863a78a37d901e61a0311097b0f6aab0b7036c9
KC Ng <nkc900201@gmail.com>
2023-08-21 21:07:19 +0900

    (WIP) adding new component - MealItemForm.tsx

---
+++ src/components/Meals/MealItemForm/MealItemForm.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+// WIP
+const MealItemForm = () => {
+  return (
+    <form>
+      <input />
+      <button>+ Add</button>
+    </form>
+  )
+}
+
+export default MealItemForm
